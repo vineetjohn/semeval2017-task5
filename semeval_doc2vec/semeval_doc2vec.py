bbaf7763ad90ade3fa5fa41a85ec6a66f01898d5
Vineet John <vinjohn10@gmail.com>
2016-10-04 19:03:35 -0400

    File reading

--- semeval_doc2vec/semeval_doc2vec.py
+++ semeval_doc2vec/semeval_doc2vec.py
@@ -1,1 +1,10 @@
-print "hello"
+import json
+
+microblog_data_path = "/home/v2john/PycharmProjects/SemevalDoc2Vec/semeval_doc2vec/semeval-2017-task-5-subtask-1/Microblog_Trialdata-full.json"
+
+with open(microblog_data_path, "r") as microblog_data_file:
+    microblog_data = microblog_data_file.read()
+
+blogpost_list = json.loads(microblog_data)
+
+print blogpost_list[0]
