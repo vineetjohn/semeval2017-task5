c8df0d2b2099efb7e955e012155b68b159ef0db2
Vineet John <vineet.john@uwaterloo.ca>
2016-12-20 13:27:56 -0500

    Added testing changes for new scoring logic

--- utils/evaluation_helper.py
+++ utils/evaluation_helper.py
@@ -8,23 +8,11 @@ log = log_helper.get_logger("EvaluationHelper")
 
 def evaluate_task_score(predicted_score_vector, gold_standard_vector):
 
-    log.info("predicted_score_vector: " + str(predicted_score_vector))
-    log.info("gold_standard_vector: " + str(gold_standard_vector))
+    log.debug("predicted_score_vector: " + str(predicted_score_vector))
+    log.debug("gold_standard_vector: " + str(gold_standard_vector))
 
-    predicted_score_norm = np.linalg.norm(predicted_score_vector)
-    gold_standard_norm = np.linalg.norm(gold_standard_vector)
-    log.info("predicted_score_norm: " + str(predicted_score_norm))
-    log.info("gold_standard_norm: " + str(gold_standard_norm))
-
-    cosine_weight = np.divide(predicted_score_norm, gold_standard_norm)
     cosine_smty = \
         cosine_similarity(np.array(predicted_score_vector).reshape(1, -1),
                           np.array(gold_standard_vector).reshape(1, -1))[0][0]
 
-    log.info("cosine_weight " + str(cosine_weight))
-    log.info("cosine_similarity " + str(cosine_smty))
-
-    final_cosine_score = cosine_weight * cosine_smty
-    log.info("final_cosine_score " + str(final_cosine_score))
-
-    return final_cosine_score
+    return cosine_smty
